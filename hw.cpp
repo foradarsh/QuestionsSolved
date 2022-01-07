59ae85ee93d653236fb945368849559530856a84
foradarsh <adarsh2002pandita@gmail.com>
2022-01-07 13:35:19 +0530

    Bubble Sort

--- hw.cpp
+++ hw.cpp
@@ -227,3 +227,25 @@ int main()
     cout << "Answer is " << ans << endl;
     return 0;
 }
+//Bubble sort using Recursion
+#include<iostream>
+using namespace std;
+
+void sortArray( int *arr, int n){
+
+    if(size == 0 || size == 1){
+        return;
+    }
+    for(int i=0;i<n-1;i++){
+        if(arr[i] > arr[i+1]){
+            swap(arr[i],arr[i+1]);
+        }
+    }
+    sortArray(arr,n-1);
+}
+int main()
+{
+     int arr[5] = {2,5,1,6,9};
+     sortArray(arr,5);
+    return 0;
+}
