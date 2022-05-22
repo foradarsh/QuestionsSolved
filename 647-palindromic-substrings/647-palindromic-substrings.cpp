9f45c9ebc54176a49ccd065d5e22f1771e8a8ddb
Kumar Adarsh Pandita <71866118+foradarsh@users.noreply.github.com>
2022-05-22 14:02:15 +0530

    Time: 5 ms (77.15%), Space: 6.2 MB (96.68%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int countSubstrings(string s) {
-        int n =s.size(), cnt =n;
-        for(int i=1,l=0,r=2;i<n;i++,l=i-1,r=i+1)
-            while(l >=0 && r<n && s[l] == s[r]) l--,r++,cnt++;
-        for(int i =1,l=0,r=1;i<n;i++,l=i-1,r=i)
-            while(l >=0 && r<n && s[l] == s[r]) l--,r++,cnt++;
-        return cnt;
+       int n =s.size(), count =n;
+		//Case 1: ODD Length Palindrome
+        for(int i=1,left=0,right=2;i<n;i++,left=i-1,right=i+1)
+            while(left>=0 && right<n && s[left] == s[right]) left--,right++,count++;
+		//Case 2: EVEN Length Palindrome
+        for(int i =1,left=0,right=1;i<n;i++,left=i-1,right=i)
+            while(left >=0 && right<n && s[left] == s[right]) left--,right++,count++;
+        return count;
     }
 };
